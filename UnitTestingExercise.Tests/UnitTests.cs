08acd7c9c19fd1c457c334537eab171b8456e447
yahel dooley <ydooley@gmail.com>
2022-11-21 21:02:21 -0800

    Completed exercise tasks and made to random Fact methods

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,80 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
-        public void Add(int num1, int num2, int num3, int expected)
+        [InlineData(1,2,3, 6)]
+		[InlineData(-1, -2, -3, -6)]
+		[InlineData(10, 15, 75, 100)]
+
+		public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var unitTestMethods = new UnitTestMethods();
             //Act
+            int actual = unitTestMethods.Add(num1, num2, num3);
 
             //Assert
-
-        }
+            Assert.Equal(actual, expected);
+		}
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(10,5,5)]
+		[InlineData(5, 10, -5)]
+		public void Subtract(int minuend, int subtrhend, int expected)
         {
-            //Arrange
+			//Arrange
+			var unitTestMethods = new UnitTestMethods();
 
-            //Act
-
-            //Assert
-
-        }
+			//Act
+			int actual = unitTestMethods.Subtract(minuend, subtrhend);
+			//Assert
+			Assert.Equal(actual, expected);
+		}
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,2,4)]
         public void Multiply(int num1, int num2, int expected)
         {
-            //Arrange
-
-            //Act
-
-            //Assert
-
-        }
+			//Arrange
+			var unitTestMethods = new UnitTestMethods();
+			//Act
+			int actual = unitTestMethods.Multiply(num1, num2);
+			//Assert
+			Assert.Equal(actual, expected);
+		}
 
         [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
+        [InlineData(10,5,2)]
+		[InlineData(60, 10, 6)]
+		public void Divide(int num1, int num2, int expected)
         {
-            //Arrange
+			//Arrange
+			var unitTestMethods = new UnitTestMethods();
+			//Act
+			float actual = unitTestMethods.Divide(num1, num2);
 
-            //Act
-
-            //Assert
-
-        }
+			//Assert
+			Assert.Equal(actual, expected);
+		}
 
         [Fact]
-        public void YourMethodName()
+        public void GetHey()
         {
-            //Arrange
-
-            //Act
-
-            //Assert
-
-        }
+			//Arrange
+			var unitTestMethods = new UnitTestMethods();
+			//Act
+			var actual = unitTestMethods.GetHey();
+			//Assert
+			Assert.Equal("Hey",actual);
+		}
 
         [Fact]
-        public void YourMethodName2()
+        public void MysteryMethod()
         {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
+			//Arrange
+			var unitTestMethods = new UnitTestMethods();
+			var actual = unitTestMethods.MysteryMethod();
+			//Assert
+			Assert.Equal("I'm mysterious", actual);
+		}
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,17 +9,39 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
 
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         // Create a Divide method that passes 2 integers
-
-
+        public float Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string GetHey()
+        {
+            return "Hey";
+		}
+        
+        public string MysteryMethod()
+        {
+            return "I'm mysterious";
+        }
 
         
     }
